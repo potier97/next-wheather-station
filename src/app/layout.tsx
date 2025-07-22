5754c48a57ec16eb6ddb21532d6fa6cccf70bbe9
Nicolas Potier Anzola <nicolaspotier97@gmail.com>
2025-07-21 23:22:19 -0500

    update custom meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="format-detection" content="telephone=no" />
       </head>
